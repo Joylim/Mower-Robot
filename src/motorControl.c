f5deebe06af679709ec955ed1884c9c99d188b86
HongboHuang <hy23thu@163.com>
2015-04-11 10:35:05 +0800

    test1

--- src/motorControl.c
+++ src/motorControl.c
@@ -1,4 +1,5 @@
 #include <wiringPi.h>
+#include <unistd.h>
 
 #define motorL1 0
 #define motorL2 1
@@ -6,6 +7,10 @@
 #define motorR1 3
 #define motorR2 4
 
+void delay(int n){
+    for (int i=0;i<n;i++)
+        for(int j=0;j<100;j++)
+}
 void motorControl_init(){
     wiringPiSetup();
 
@@ -18,29 +23,31 @@ void motorControl_init(){
     digitalWrite(motorL2,LOW);
     digitalWrite(motorR1,LOW);
     digitalWrite(motorR2,LOW);
-    //delay(10);
+    delay(10);
 }
 
 void leftForward(){
     digitalWrite(motorL1,HIGH);
     digitalWrite(motorL2,LOW);
-    //delay(10);
+    delay(10);
 }
 
 void rightForward(){
     digitalWrite(motorR1,HIGH);
     digitalWrite(motorR2,LOW);
-    //delay(10);
+    delay(10);
 }
 
 void leftBack(){
     digitalWrite(motorL1,LOW);
     digitalWrite(motorL2,HIGH);
+    delay(10);
 }
 
 void rightBack(){
     digitalWrite(motorR1,LOW);
     digitalWrite(motorR2,HIGH);
+    delay(10);
 }
 //function
 
@@ -48,26 +55,33 @@ void robotForward(){
     motorControl_init();
     leftForward();
     rightForward();
-    //delay(20);
+    delay(10);
 }
 
 void robotBack(){
     motorControl_init();
     leftBack();
     rightBack();
-    //delay(20);
+    delay(10);
 }
 
 void robotLeft(){
     motorControl_init();
     leftBack();
     rightForward();
-    //delay(20);
+    delay(10);
 }
 
 void robotRight(){
     motorControl_init();
     leftForward();
     rightBack();
-    //delay(20);
+    delay(10);
+}
+
+int main()
+{
+    robotForward();
+    sleep(200);
+    return 0;
 }
