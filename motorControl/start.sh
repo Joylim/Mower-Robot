9627688823118150f0d8e6ab00ed9b1ebd4531f1
root <root@raspberrypi.(none)>
2015-06-02 01:02:33 +0800

    update start shell

--- motorControl/start.sh
+++ motorControl/start.sh
@@ -1,6 +1,13 @@
+echo "rm -rf build/"
 rm -rf build/
+echo "mkdir build"
 mkdir build
+echo "cd build/"
 cd build/
+echo "cmake --prefix=/usr/local"
 cmake -DCMAKE-INSTALL-PREFIX=/usr/local ..
-
-echo "please make and make install "
+echo "make"
+make
+echo "make install"
+make install
+echo "build motorcontrol.a motorcontrol.so successful! and the files is in /usr/local/lib "
