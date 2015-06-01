4e8fc8bbb42ad789bc6a4cbe44e47c7950ce36f1
root <root@raspberrypi.(none)>
2015-06-01 14:06:22 +0800

    libmotorcontrol.so

---
+++ motorControl/build/lib/CMakeFiles/motorcontrol_static.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/motorcontrol_static.dir/motorControl.o"
+  "libmotorcontrol.pdb"
+  "libmotorcontrol.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/motorcontrol_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ motorControl/lib/motorControl.h
@@ -0,0 +1,39 @@
+#include <wiringPi.h>
+#include <unistd.h>
+
+#define MOTORL1 0
+#define MOTORL2 1
+
+#define MOTORR1 3
+#define MOTORR2 4
+
+int LEFT = -1;
+int RIGHT = -1;
+int BACK = -1;
+int FORWARD = -1;
+
+/*
+  speed = 1: slow
+  speed = 2: normal
+  speed = 3: fast
+*/
+int SPEED = 2;
+
+void delay100(int n);
+void motorControl_init();
+
+void leftForward();
+void rightForward();
+void leftBack();
+void rightBack();
+
+void robotForward();
+void robotBack();
+void robotLeft();
+void robotRight();
+
+
+//void robotRun();
+void robotStop();
+void robotStart();
+void robotPause();
